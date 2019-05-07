9ae2af8188e8d6a5524d988f11d9277deb70239b
Beer Pratap Singh <beerpratap.s@turtlemint.com>
2019-05-07 15:44:37 +0530

    commit changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Ninjas from './Ninjas';
+import AddNinjaForm from './AddNinjaForm';
 
 class App extends Component {
-  render() {
+//  constructor(props) {
+//    super(props);
+//    this.state = {
+//      name: 'Harsh Singh',
+//      email: 'test@test.com',
+//      address: 'test address'
+//    }
+//  }  
+ state = {
+   ninjas : [
+     {name: 'John', age: 30, email: 'john@test.com', id: 1},
+     {name: 'Ryan', age: 27, email: 'ryan@test.com', id: 2},
+     {name: 'Sam', age: 25, email: 'sam@test.com', id: 3},
+   ]
+ } 
+
+  addNinja = (ninja) => {
+    ninja.id = Math.random();
+    let ninjas = [...this.state.ninjas, ninja];
+    this.setState({
+      ninjas : ninjas
+    })
+  }
+
+  deleteNinja = (id) => {
+    console.log(id);
+    let ninjas = this.state.ninjas.filter(ninja => {
+      return ninja.id !== id;
+    });
+    this.setState({
+      ninjas : ninjas
+    })
+    
+  }
+
+  render() {     
+    // setTimeout(() => {
+    //   this.setState({
+    //     name: 'New Name'
+    //   });
+    // }, 20000);
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>Welcome to Ninja</h1>
+        <p>Welcome :)</p>
+        {/* <Ninjas name={this.state.name} email={this.state.email} address={this.state.address} />         */}
+        <Ninjas deleteNinja={this.deleteNinja} ninjas={this.state.ninjas} /> 
+        <AddNinjaForm addNinja={this.addNinja}/>                    
       </div>
     );
   }
